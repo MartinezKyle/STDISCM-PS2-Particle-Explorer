9d30b7f9fb303ed4bc5d1482f2c99a4dab1c3efa
Kyle Martinez <kyle.k.martinez@gmail.com>
2024-04-15 01:11:13 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# STDISCM-PS2-Particle-Explorer
+# Problem Set #2: Concurrent Programming w/ Dependencies
+## Members
+- [ABALOS, Alyanna](https://github.com/alyannaabalos)
+- [BONDOC, Alyana Erin](https://github.com/Alybee75)
+- [MARTINEZ, Kyle](https://github.com/Kyle-Martinez)
+- [PIANO, Gabriel Edward](https://github.com/Poije)
+
+## How to Run
+
+- Open the `.jar` file in the main project folder. Or in a command line interface on the project main folder, use the command `java -jar ParticleExplorer.jar`
+
+## How to Use
+### Switching Between Modes 
+- To switch between developer and explorer modes, click on the desired mode on the toggle button on top of the control panel.
+### Developer Mode
+- To add a single particle, ensure that the program is in developer mode, no checkbox is enabled, and the first four fields are all filled out.
+- To add multiple balls, ensure that the program is in developer mode, the checkbox beside the option is checked to be used first to enable the other input fields, and all the enabled input fields are filled out.
+### Explorer Mode
+- To spawn a sprite, just click on the panel where the sprite should be placed.
+- To move the sprite around the panel, use the `W`, `A`, `S`, and `D` keys to move the sprite up, left, down, and right, respectively.
+### Stopping the Program
+- To stop the program, close the program
---
+++ src/Particle.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+
+public class Particle extends JComponent{
+    private double x_coord;
+    private double y_coord;
+    private double velocity;
+    private double angle;
+
+    public Particle(double x, double y, double velocity, double angle){
+        this.x_coord = x;
+        this.y_coord = y;
+        this.angle = angle;
+        this.velocity = velocity;
+        repaint();
+    }
+
+    public void updatePosition(double time){
+        double x2 = x_coord + getVelocityX() * time;
+        double y2 = y_coord + getVelocityY() * time;
+        if (x2 <= 0 || x2 >= 1280){
+            this.angle = 180 - angle; 
+        }
+        if (y2 <= 0 || y2 >= 720){
+            this.angle = - angle;
+        }
+        x_coord += getVelocityX() * time;
+        y_coord += getVelocityY() * time;
+        repaint();
+    }
+
+    public double getXCoord(){
+        return x_coord;
+    }
+
+    public double getYCoord() {
+        return y_coord;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+    public double getVelocityX() {
+        return velocity * Math.cos(Math.toRadians(angle));
+    }
+    public double getVelocityY() {
+        return velocity * Math.sin(Math.toRadians(angle));
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillOval((int) x_coord - 5, 720 - (int)y_coord - 5, 10, 10);
+    }
+}
